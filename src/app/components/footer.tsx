39c66a4b9a66330b07b1d4a626227ed091b4289c
rem <ultraburgundy@gmail.com>
2023-08-29 16:07:34 -0400

    adjusted

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -2,9 +2,9 @@ const Footer = () => {
 
   return (
     <footer className="py-8">
-      <div className="border-t-2 border-stone-400">
-        <p className="mt-6 text-xs font-bold text-center text-stone-600">
-          &copy; 2023 Menu, curated by <a href="https://rem-portfolio.vercel.app"><i className="text-red-400 underline">Rem</i></a>
+      <div className="border-t-2 border-[#5E313E]">
+        <p className="mt-6 text-xs font-bold text-center text-[#5E313E]">
+          &copy; 2023 Menu, curated by <a href="https://rem-portfolio.vercel.app"><i className="text-white font-extrabold underline">Rem</i></a>
         </p>
       </div>
       
